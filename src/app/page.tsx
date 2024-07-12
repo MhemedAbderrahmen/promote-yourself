906cd4f425734aa7e5a2b0aef693fbab3096f536
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-07-12 15:42:39 +0100

    fix: topnav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { MegaphoneIcon } from "lucide-react";
 import { ModeToggle } from "~/components/mode-toggle";
 import { HydrateClient } from "~/trpc/server";
 
 export function TopNav() {
   return (
-    <nav className="flex w-full flex-row items-center justify-between border-b-2 p-4">
-      <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
-        promotething
-      </h3>
-      <div>
-        <ModeToggle />
-      </div>
-    </nav>
+    <div className="flex flex-row items-center justify-center border-b-2">
+      <nav className="flex w-full max-w-screen-lg flex-row items-center justify-between p-4">
+        <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
+          promote<span className="text-green-500">thing</span>
+        </h3>
+        <div>
+          <ModeToggle />
+        </div>
+      </nav>
+    </div>
   );
 }
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex h-full w-full items-center justify-between">
-        <TopNav />
-      </main>
+      <TopNav />
+      <main className="flex h-full w-full items-center justify-between"></main>
     </HydrateClient>
   );
 }
