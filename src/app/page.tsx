4e4c6f82624d9af7caac8a05b63f6b7246acab72
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2024-07-12 20:24:54 +0200

    fix issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 import TopNav from "./_components/top-nav";
+import Listings from "./_components/listings";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -12,7 +12,7 @@ export default async function Home() {
       <TopNav />
       <main className="flex w-full flex-row items-center justify-center">
         <div className="flex w-full max-w-screen-lg flex-row flex-wrap gap-2 p-4">
-          <LatestPost />
+          <Listings />
         </div>
       </main>
     </HydrateClient>
